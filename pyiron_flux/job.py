dcc0d5310e02c1bdeb6ad6b18df6be16704b3c77
Marvin Poul <poul@mpie.de>
2022-10-04 13:35:38 +0200

    Let MlipFactory dervie from JobFactory

--- pyiron_flux/job.py
+++ pyiron_flux/job.py
@@ -109,64 +109,20 @@ class VaspFactory(JobFactory):
         )
         return job
 
-class MlipFactory(HasHDF):
-
-    def __init__(self, project=None):
-        self._project = project
-        self._potential = None
-
-    @property
-    def project(self):
-        return self._project
-
-    @project.setter
-    def project(self, value):
-        self._project = value
+class MlipFactory(JobFactory):
 
     @property
     def potential(self):
-        return self._potential
+        return self.storage.potential
 
     @potential.setter
     def potential(self, value):
-        self._potential = value
+        self.storage.potential = value
 
-    @property
-    def hamilton(self):
+    def _get_hamilton(self):
         return "LammpsMlip"
 
     def _prepare_job(self, job, structure):
-        job.structure = structure
+        super()._prepare_job(job, structure)
         job.potential = self.potential
         return job
-
-    def run(self,
-            name: str, modify: Callable[[GenericJob], GenericJob],
-            structure: Atoms,
-            delete_existing_job=False, delete_aborted_job=True
-    ) -> Optional[GenericJob]:
-
-        # short circuit if job already successfully ran
-        if name in self.project.list_nodes() \
-                and self.project.get_job_status(name) == 'finished':
-            return None
-
-        job = self.project.create.job.LammpsMlip(
-                name,
-                delete_existing_job=delete_existing_job,
-                delete_aborted_job=delete_aborted_job
-        )
-        if not job.status.initialized: return job
-
-        job = self._prepare_job(job, structure)
-        job = modify(job)
-
-        with open('/dev/null', 'w') as f, contextlib.redirect_stdout(f):
-            job.run()
-        return job
-
-    def _to_hdf(self, hdf):
-        hdf['potential'] = self.potential
-
-    def _from_hdf(self, hdf, version=None):
-        self.potential = hdf['potential']
