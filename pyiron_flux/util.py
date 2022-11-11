bbcc080bd40ce36ef23a24dfdc9b719cc7bab881
Marvin Poul <ponder@creshal.de>
2022-11-11 11:44:29 +0100

    Move symlink_project from potfit

--- pyiron_flux/util.py
+++ pyiron_flux/util.py
@@ -1,3 +1,5 @@
+from pyiron_base import Project
+import os.path
 
 def get_table(pr, table_name, add, delete_existing_job=False):
     if table_name in pr.list_nodes() and not delete_existing_job:
@@ -7,3 +9,11 @@ def get_table(pr, table_name, add, delete_existing_job=False):
         add(tab)
         tab.run()
         return tab
+
+
+def symlink_project(pr: Project):
+        target_dir = pr.project_path
+        if target_dir[-1] == '/':
+            target_dir = target_dir[:-1]
+        pr.symlink(os.path.join('/cmmc/ptmp', os.path.dirname(target_dir)))
+
